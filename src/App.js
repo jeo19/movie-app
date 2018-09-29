5a0407a00e6a6129701f400a7ef908b8508e2d16
jeo19 <terrakkk@outlook.com>
2018-09-29 23:28:30 +0630

    Lifecycle events for React'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './Movie.css';
+import Movie from './Movie';
+const moives = [
+  {
+    title: 'Star wars',
+    poster:
+      'https://c.ndtvimg.com/star-wars-instagram_625x300_1526457978394.jpg?output-quality=70&output-format=webp'
+  },
+  {
+    title: 'The old boys',
+    poster:
+      'https://m.media-amazon.com/images/M/MV5BOTM5MjEzNDAzN15BMl5BanBnXkFtZTcwNjg1OTk5OA@@._V1_SY1000_CR0,0,706,1000_AL_.jpg'
+  },
+  {
+    title: 'Spider man',
+    poster:
+      'https://superclub.videotron.com/media/catalog/product/cache/image/500x711/e9c3970ab036de70892d86c6d221abfe/v/a/va_14.png'
+  },
+  {
+    title: 'Bat man',
+    poster:
+      'https://http2.mlstatic.com/batman-arkham-knight-premium-edition-34-dlcs-envio-imediato-D_NQ_NP_792976-MLB26931079202_022018-F.webp'
+  }
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {moives.map((movie, index) => {
+          return (
+            <Movie title={movie.title} poster={movie.poster} key={index} />
+          );
+        })}
       </div>
     );
   }
