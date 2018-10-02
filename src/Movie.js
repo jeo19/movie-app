7d651ff48e9f4d1487998bebfb9e00502819b49e
jeo19 <terrakkk@outlook.com>
2018-10-03 00:30:45 +0630

    Dumb Components without state

--- src/Movie.js
+++ src/Movie.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
-class Movie extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-  };
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+
+function Movie({ title, poster }) {
+  return (
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  );
 }
-class MoviePoster extends Component {
-  static propTypes = {
-    poster: PropTypes.string.isRequired
-  };
-  render() {
-    return <img src={this.props.poster} alt="props-text" />;
-  }
+function MoviePoster({ poster }) {
+  return <img src={poster} alt="props-text" />;
 }
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+};
 export default Movie;
